4fd99c972f571121db2f17ddbf4c07a36d84c9e3
kroffo <kroffo@oswego.edu>
2017-04-20 14:13:13 -0400

    Fixed spelling error.

--- grails-app/domain/grailsstockmarketsimulator/User.groovy
+++ grails-app/domain/grailsstockmarketsimulator/User.groovy
@@ -6,10 +6,8 @@ class User {
     String password
     double money
 
-    static hasMany = [company: Company]
-
     static constraints = {
         userName(blank: false, size: 1..20, unique: true)
-        password(blank: flase, size: 1..30)
+        password(blank: false, size: 1..30)
     }
 }
